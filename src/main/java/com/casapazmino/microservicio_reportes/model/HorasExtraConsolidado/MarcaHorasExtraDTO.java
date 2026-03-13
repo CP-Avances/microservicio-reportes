0c33c5890339a32069bb0abe8ca7dd4f76a40bef
cen <98037053+CP-Avances@users.noreply.github.com>
2026-03-13 10:47:32 -0500

    reporteHorasExtra

---
+++ src/main/java/com/casapazmino/microservicio_reportes/model/HorasExtraConsolidado/MarcaHorasExtraDTO.java
@@ -0,0 +1,16 @@
+package com.casapazmino.microservicio_reportes.model.HorasExtraConsolidado;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MarcaHorasExtraDTO {
+    private String fecha_horario;
+    private String fecha_hora_horario;
+    private String fecha_hora_timbre;
+}
